3542d9dd4cb89fab287b92994690be09b1434e53
Petrus Handika <petrushandikasinaga@gmail.com>
2025-05-24 15:01:56 +0700

    fix: fix code

--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -1,3 +1,4 @@
+// src/routes/auth.route.ts
 import express from "express";
 import * as AuthController from "../controllers/auth.controller";
 import passport from "passport";
@@ -23,7 +24,30 @@ router.get(
     const user = req.user as any;
 
     if (!user) {
-      return res.redirect("http://localhost:5173/auth/login");
+      return res.redirect(`${process.env.VITE_URL}/auth/login`);
+    }
+
+    const token = jwt.sign({ userId: user.id }, process.env.JWT_SECRET!, {
+      expiresIn: "1h",
+    });
+
+    res.redirect(`${process.env.VITE_URL}/auth/callback?token=${token}`);
+  }
+);
+
+router.get("/facebook", passport.authenticate("facebook", { scope: "email" }));
+
+router.get(
+  "/facebook/callback",
+  passport.authenticate("facebook", {
+    failureRedirect: `${process.env.VITE_URL}/auth/login`,
+    session: false,
+  }),
+  (req, res) => {
+    const user = req.user as any;
+
+    if (!user) {
+      return res.redirect(`${process.env.VITE_URL}/auth/login`);
     }
 
     const token = jwt.sign({ userId: user.id }, process.env.JWT_SECRET!, {
